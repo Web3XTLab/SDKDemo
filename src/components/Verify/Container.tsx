084946da0f6934d390f782fbea4271ba739a6171
Soulike <soulike@qq.com>
2022-06-14 16:03:51 +0800

    feat: finish Verify

--- src/components/Verify/Container.tsx
+++ src/components/Verify/Container.tsx
@@ -1,17 +1,52 @@
-import {ITextFieldProps} from '@fluentui/react';
+import {isAuthenticated} from '@/lib/dapp-store-verify';
+import {IButtonProps, ITextFieldProps} from '@fluentui/react';
 import React, {useState} from 'react';
 import {VerifyView} from './View';
 
-export function VerifyContainer()
+export function Verify()
 {
+    const [loading, setLoading] = useState(false);
     const [appTokenId, setAppTokenId] = useState('');
+    const [resultText, setResultText] = useState('');
 
     const onAppTokenIdChange: ITextFieldProps['onChange'] = e =>
     {
         setAppTokenId(e.currentTarget.value);
-    }
+    };
 
-    
+    const onButtonClick: IButtonProps['onClick'] = async () =>
+    {
+        const appTokenIdNumber = Number.parseInt(appTokenId);
+        if (Number.isNaN(appTokenIdNumber))
+        {
+            setResultText('Invalid app token id, please check again.');
+        }
+        else
+        {
+            setLoading(true);
+            const result = await isAuthenticated(appTokenIdNumber);
+            setLoading(false);
+            if (result)
+            {
+                setResultText('The current consumer passes the authentication.');
+            }
+            else if(result === false)
+            {
+                setResultText('The current consumer does not pass the authentication.');
+            }
+            else
+            {
+                setResultText('Something goes wrong...');
+            }
+        }
+    };
 
-    return (<VerifyView appTokenId={appTokenId} onAppTokenIdChange={onAppTokenIdChange} />);
+
+    return (
+        <VerifyView
+            loading={loading}
+            appTokenId={appTokenId}
+            resultText={resultText}
+            onAppTokenIdChange={onAppTokenIdChange}
+            onButtonClick={onButtonClick} />);
 }
--- src/components/Verify/View.tsx
+++ src/components/Verify/View.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import {ITextFieldProps, Label, TextField} from '@fluentui/react';
+import {ITextFieldProps, TextField, PrimaryButton, IButtonProps} from '@fluentui/react';
+import {Title} from '../Title';
 
 export interface IVerifyViewProps
 {
+    loading: boolean;
     appTokenId: string;
+    resultText: string;
     onAppTokenIdChange: ITextFieldProps['onChange'];
+    onButtonClick: IButtonProps['onClick'];
 }
 
 export function VerifyView(props: IVerifyViewProps)
 {
-    const {appTokenId, onAppTokenIdChange} = props;
+    const {appTokenId, loading, resultText, onAppTokenIdChange, onButtonClick} = props;
     return (
         <div className={styles.Verify}>
-            <Label>
-                Verify Authentication
-            </Label>
-            <TextField label={'App Token Id'} value={appTokenId} onChange={onAppTokenIdChange} />
+            <Title>Verify Authentication</Title>
+            <TextField disabled={loading} placeholder={'Enter app token id'} value={appTokenId} onChange={onAppTokenIdChange} />
+            <div className={styles.buttonWrapper}>
+                <PrimaryButton disabled={loading} className={styles.button} onClick={onButtonClick}>Verify</PrimaryButton>
+            </div>
+            <div className={styles.resultText}>{resultText}</div>
         </div>);
 }

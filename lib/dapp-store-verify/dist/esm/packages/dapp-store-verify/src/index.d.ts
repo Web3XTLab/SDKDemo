4613e287c9f05936d3c36d22a7b5548ef59d5b78
Soulike <soulike@qq.com>
2022-06-14 16:25:10 +0800

    fix: upload lib

---
+++ lib/dapp-store-verify/dist/esm/packages/dapp-store-verify/src/index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * @param appTokenId - The token id of the app
+ * @returns Whether the verified consumer has bought the app
+ */
+export declare function isAuthenticated(appTokenId: number): Promise<boolean>;

267fb89dfadacb77d3ef6454ae22a7b07c3c7186
Soulike <soulike@qq.com>
2022-06-16 09:39:12 +0800

    dep: update abis and web3app.js

--- src/utils/web3app.js
+++ src/utils/web3app.js
@@ -12,30 +12,38 @@ const App = {
 
   toBN: Web3.utils.toBN,
 
-  init: async () => {
+  init: async () =>
+  {
     if (App.web3) return App;
     return await App.initWeb3();
   },
 
-  initWeb3: async () => {
-    try {
+  initWeb3: async () =>
+  {
+    try
+    {
       // Modern dapp browsers...
-      if (window.ethereum) {
+      if (window.ethereum)
+      {
         App.web3Provider = window.ethereum;
-        try {
+        try
+        {
           // Request account access
-          await window.ethereum.request({ method: "eth_requestAccounts" });
-        } catch (error) {
+          await window.ethereum.request({method: "eth_requestAccounts"});
+        } catch (error)
+        {
           // User denied account access...
           console.error("User denied account access");
         }
       }
       // Legacy dapp browsers...
-      else if (window.web3) {
+      else if (window.web3)
+      {
         App.web3Provider = App.web3.currentProvider;
       }
       // IF no injected web3 instance is detected, fall back to Ganache
-      else {
+      else
+      {
         App.web3Provider = new Web3.providers.HttpProvider(
           "http://localhost:7545"
         );
@@ -44,13 +52,15 @@ const App = {
       App.web3 = new Web3(App.web3Provider);
 
       return await App.initContract();
-    } catch (e) {
+    } catch (e)
+    {
       console.error(e);
       return null;
     }
   },
 
-  initContract: async () => {
+  initContract: async () =>
+  {
     const networkId = await App.web3.eth.net.getId();
     console.log("networkId", networkId);
 
@@ -61,19 +71,23 @@ const App = {
       nftDeployedNetwork.address
     );
 
-    try {
+    try
+    {
       await App.getAccount();
-      window.ethereum.once("accountsChanged", (accounts) => {
+      window.ethereum.once("accountsChanged", (accounts) =>
+      {
         location.reload();
       });
-    } catch (e) {
+    } catch (e)
+    {
       console.error(e);
     }
 
     return App;
   },
 
-  getAccount: async () => {
+  getAccount: async () =>
+  {
     if (App.account) return App.account;
     const accounts = await App.web3.eth.getAccounts();
     App.account = accounts[0];
@@ -82,106 +96,147 @@ const App = {
 
   // TODO: https://docs.metamask.io/guide/ethereum-provider.html
   // import detectEthereumProvider from '@metamask/detect-provider';
-  useMetaMask: () => {
+  useMetaMask: () =>
+  {
     return !!window.ethereum;
   },
 
-  sell: async (tokenURI, amount) => {
-    try {
+  sell: async (name, tokenURI, price) =>
+  {
+    try
+    {
       const account = await App.getAccount();
       console.log("account", account);
 
       App.contracts.AppStore.events
         .OnSell()
-        .on("data", (event) => {
+        .on("data", (event) =>
+        {
           console.log("sell", event);
         })
-        .on("error", (error) => {
+        .on("error", (error) =>
+        {
           console.log(error);
         });
 
       return await App.contracts.AppStore.methods
-        .sell(amount, tokenURI)
-        .send({ from: account });
-    } catch (e) {
+        .sell(name, tokenURI, price)
+        .send({from: account});
+    } catch (e)
+    {
       console.error(e);
     }
   },
 
-  buy: async (tokenId, amount) => {
-    try {
+  buy: async (tokenId, price) =>
+  {
+    try
+    {
       const account = await App.getAccount();
       console.log("account", account);
 
       App.contracts.AppStore.events
         .OnBuy()
-        .on("data", (event) => {
+        .on("data", (event) =>
+        {
           console.log("buy", event);
         })
-        .on("error", (error) => {
+        .on("error", (error) =>
+        {
           console.log(error);
         });
 
       return await App.contracts.AppStore.methods
         .buy(tokenId)
-        .send({ from: account, value: amount });
-    } catch (e) {
+        .send({from: account, value: price});
+    } catch (e)
+    {
       console.error(e);
     }
   },
 
-  verify: async (tokenId) => {
-    try {
+  verify: async (tokenId) =>
+  {
+    try
+    {
       const account = await App.getAccount();
       console.log("account", account);
 
       App.contracts.AppStore.events
         .OnVerify()
-        .on("data", (event) => {
+        .on("data", (event) =>
+        {
           console.log("verify", event.returnValues.verified);
         })
-        .on("error", (error) => {
+        .on("error", (error) =>
+        {
           console.log(error);
         });
 
       return await App.contracts.AppStore.methods
         .verify(tokenId, account)
         .call();
-    } catch (e) {
+    } catch (e)
+    {
       console.error(e);
     }
   },
 
-  totalCount: async () => {
-    try {
+  totalCount: async () =>
+  {
+    try
+    {
       return await App.contracts.AppStore.methods.totalCount().call();
-    } catch (e) {
+    } catch (e)
+    {
       console.error(e);
     }
   },
 
-  tokenURI: async (tokenId) => {
-    try {
+  tokenURI: async (tokenId) =>
+  {
+    try
+    {
       return await App.contracts.AppStore.methods
         .getTokenURI(tokenId)
         .call();
-    } catch (e) {
+    } catch (e)
+    {
       console.error(e);
     }
   },
 
-  tokenURIs: async () => {
-    try {
+  tokenURIs: async () =>
+  {
+    try
+    {
       const items = [];
       const count = await App.contracts.AppStore.methods.totalCount().call();
-      for (let i = 0; i < count; i++) {
+      for (let i = 0; i < count; i++)
+      {
         const item = await App.contracts.AppStore.methods.getTokenURI(i).call();
         items.push(item);
       }
       return items;
-    } catch (e) {
+    } catch (e)
+    {
       console.error(e);
     }
+  },
+
+  getAppInfo: async (tokenId) =>
+  {
+    const name = await App.contracts.AppStore.methods.getAppName(tokenId).call();
+    const price = await App.contracts.AppStore.methods.getAppPrice(tokenId).call();
+    const seller = await App.contracts.AppStore.methods.getAppSeller(tokenId).call();
+    const buyers = await App.contracts.AppStore.methods.getAppBuyers(tokenId).call();
+
+    return {
+      name,
+      price,
+      seller,
+      buyers,
+    };
   },
 };
 

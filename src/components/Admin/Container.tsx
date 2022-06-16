1bfc743d93f74314fade9d7f8eaa4411396a658c
Soulike <soulike@qq.com>
2022-06-16 11:15:00 +0800

    feat: move Web3AppContext into Admin

--- src/components/Admin/Container.tsx
+++ src/components/Admin/Container.tsx
@@ -1,5 +1,8 @@
+import {Web3AppContext} from '@/src/contexts/Web3AppContext';
 import Head from 'next/head';
+import {Web3AppInitFrame} from '../Web3AppInitFrame';
 import {AdminView} from './View';
+import web3app from '../../utils/web3app';
 
 export function Admin()
 {
@@ -8,7 +11,11 @@ export function Admin()
             <Head>
                 <title>Admin Page</title>
             </Head>
-           <AdminView/>
+            <Web3AppContext.Provider value={web3app}>
+                <Web3AppInitFrame>
+                    <AdminView />
+                </Web3AppInitFrame>
+            </Web3AppContext.Provider>
         </>
     );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,9 @@
 import '../styles/globals.css';
 import {AppProps} from 'next/app';
-import {Web3AppContext} from '../contexts/Web3AppContext';
-import web3app from '../utils/web3app';
-import {Web3AppInitFrame} from '../components/Web3AppInitFrame';
 
 function App({Component, pageProps}: AppProps)
 {
-  return (
-    <Web3AppContext.Provider value={web3app}>
-      <Web3AppInitFrame>
-        <Component {...pageProps} />
-      </Web3AppInitFrame>
-    </Web3AppContext.Provider>
-  );
+  return (<Component {...pageProps} />  );
 }
 
 export default App;

3f7900c359c265ffe3892d5a7fbd9c8a1d970d39
Soulike <git_anonymous@foxmail.com>
2022-07-28 17:48:52 +0800

    feat: make ownership able to use basic version

--- src/components/Calculator/View.tsx
+++ src/components/Calculator/View.tsx
@@ -26,55 +26,53 @@ export function CalculatorView(props: ICalculatorViewProps) {
         loading,
         onKeyClickFactory,
     } = props;
+
+    const canUse =
+        verifyResult[APP_PLAN_TYPE.BASIC] ||
+        verifyResult[APP_PLAN_TYPE.PRO] ||
+        verifyResult[APP_PLAN_TYPE.OWNERSHIP];
+
     return (
         <div className={styles.Calculator}>
             {loading ? (
                 <Spinner label={'Loading...'} size={SpinnerSize.large} />
-            ) : verifyResult[APP_PLAN_TYPE.BASIC] ? (
-                    <div className={styles.container}>
-                        {verifyResult[APP_PLAN_TYPE.BASIC] && (
-                            <MessageBar messageBarType={MessageBarType.success}>
-                                You have acquired basic version of the app.
-                            </MessageBar>
-                        )}
-                        {verifyResult[APP_PLAN_TYPE.PRO] && (
-                            <MessageBar messageBarType={MessageBarType.success}>
-                                You have acquired pro version of the app.
-                            </MessageBar>
-                        )}
-                        {verifyResult[APP_PLAN_TYPE.OWNERSHIP] && (
-                            <MessageBar messageBarType={MessageBarType.success}>
-                                You have acquired ownership of the app.
-                            </MessageBar>
-                        )}
-                        <div className={styles.calculatorWrapper}>
-                            <div className={styles.screenWrapper}>
-                                <Screen
-                                    value={screenValue}
-                                    isShowingResult={isShowingResult}
-                                />
-                            </div>
-                            <div className={styles.keyboardWrapper}>
-                                <Keyboard
-                                    onKeyClickFactory={onKeyClickFactory}
-                                    isProVersion={
-                                        verifyResult[APP_PLAN_TYPE.PRO]
-                                    }
-                                />
-                            </div>
-                        </div>
-                    </div>
-            ) : (
+            ) : canUse ? (
                 <div className={styles.container}>
-                    <MessageBar messageBarType={MessageBarType.error}>
-                        Acquire the basic plan of the app to
-                        continue.
-                    </MessageBar>
+                    {verifyResult[APP_PLAN_TYPE.BASIC] && (
+                        <MessageBar messageBarType={MessageBarType.success}>
+                            You have acquired basic version of the app.
+                        </MessageBar>
+                    )}
+                    {verifyResult[APP_PLAN_TYPE.PRO] && (
+                        <MessageBar messageBarType={MessageBarType.success}>
+                            You have acquired pro version of the app.
+                        </MessageBar>
+                    )}
                     {verifyResult[APP_PLAN_TYPE.OWNERSHIP] && (
                         <MessageBar messageBarType={MessageBarType.success}>
                             You have acquired ownership of the app.
                         </MessageBar>
                     )}
+                    <div className={styles.calculatorWrapper}>
+                        <div className={styles.screenWrapper}>
+                            <Screen
+                                value={screenValue}
+                                isShowingResult={isShowingResult}
+                            />
+                        </div>
+                        <div className={styles.keyboardWrapper}>
+                            <Keyboard
+                                onKeyClickFactory={onKeyClickFactory}
+                                isProVersion={verifyResult[APP_PLAN_TYPE.PRO]}
+                            />
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <div className={styles.container}>
+                    <MessageBar messageBarType={MessageBarType.error}>
+                        Acquire the basic plan of the app to continue.
+                    </MessageBar>
                 </div>
             )}
         </div>
